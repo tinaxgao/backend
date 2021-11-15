562fbbbd1a7f612f1f55d05ad62cea53b17ef059
monicasalas86 <monicsalas167@gmail.com>
2021-11-15 15:21:17 -0600

    items seed ran successfully

--- api/data/seeds/05-items.js
+++ api/data/seeds/05-items.js
@@ -1,8 +1,9 @@
 exports.seed = async function(knex) {
-  await knex('potluck_users').insert([
+  await knex('items').insert([
     {
       potluck_id: 1,
-      user_id: 1
+      user_id: 1,
+      item_name: 'turkey'
     }
   ])
 }
