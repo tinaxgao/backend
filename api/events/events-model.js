075f295f6ddc5ca7521ed965c2cec35132dcaddb
monicasalas86 <monicsalas167@gmail.com>
2021-11-17 03:23:23 -0600

    fixed formatting for events

--- api/events/events-model.js
+++ api/events/events-model.js
@@ -7,7 +7,26 @@ module.exports = {
 }
 
 async function get() {
-  return await db('events')
+  const allEvents = await db('events as e')
+    .join('users as u', 'e.organizer', 'u.user_id')
+    .select(
+      'e.event_id',
+      'u.first_name as organizer',
+      'e.event_title',
+      'e.event_location',
+      'e.event_description'
+    )
+  const eventArray = allEvents.map((event) => {
+    const eventFormat = {
+      event_id: event.event_id,
+      organizer: event.organizer,
+      title: event.event_title,
+      location: event.event_location,
+      description: event.event_description
+    }
+    return eventFormat
+  })
+  return eventArray
 }
 
 async function getById(id) {
