40dc4899a901450180c7ab76b0d27bb6d91a1833
monicasalas86 <monicsalas167@gmail.com>
2021-11-17 19:53:30 -0600

    made restricted middleware

---
+++ api/restricted-middleware.js
@@ -0,0 +1,23 @@
+const {JWT_SECRET} = require('./secrets')
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization
+  if(!token) return next({
+    status: 401,
+    message: 'token required'
+  })
+
+  jwt.verify(
+    token,
+    JWT_SECRET,
+    (err, decoded) => {
+      if(err) return next({
+        status: 401,
+        message: 'token invalid'
+      })
+      req.decodedJwt = decoded
+      next()
+    }
+  )
+}
